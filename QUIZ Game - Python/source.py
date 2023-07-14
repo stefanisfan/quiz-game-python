e2f78bbbe837500d85ee6d6513fb912b2ce27676
Isfan Stefan <137828142+stefanisfan@users.noreply.github.com>
2023-07-14 18:58:53 +0300

    Update source.py

--- QUIZ Game - Python/source.py
+++ QUIZ Game - Python/source.py
@@ -1,7 +1,7 @@
-from database import intrebari
+from database import questions
 from preprocess import preprocess_data
 
-# Test grila - 18 intrebari (Matematica, Fizica si Informatica)
+# Grid test - 18 questions (Mathematics, Physics and Computer Science)
 def main():
     answers = ("D", "B", "B", "C", "A", "B", "A", "D", "C", "C", "A", "D", "A", "C", "B", "D", "D", "A")
     guesses = []
@@ -14,27 +14,27 @@ def main():
         print(k)
         print(v)
         print(', '.join(v))
-        guess = input("Apasati (A, B, C, D): ").upper()
+        guess = input("Enter (A, B, C, D): ").upper()
         guesses.append(guess)
         if guess == answers[question_num]:
             score += 1
-            print("Raspunsul este corect! ")
+            print("The answer is corect! ")
         else:
-            print("Raspunsul este gresit! ")
-            print(f"{answers[question_num]} este raspunsul corect")
+            print("The answer is wrong! ")
+            print(f"{answers[question_num]} is the correct answer")
         question_num += 1
 
     print("\n------------------------------------------")
-    print("-------------Rezultatul final-------------")
+    print("----------------Final Result----------------")
     print("------------------------------------------\n")
 
-    print("Raspunsuri: ")
+    print("Answers: ")
     for answer in answers:
         print(answer, end=" ")
 
-    print("\n\nGhicitori:")
+    print("\n\nGuesses:")
     for guess in guesses:
         print(guess, end=" ")
 
-    score = int(score / len(intrebari) * 100)
-    print(f"\nScorul este: {score}")
+    score = int(score / len(questions) * 100)
+    print(f"\nThe score is: {score}")
